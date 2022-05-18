a71448c213de7a29a3f8265391b195771cdcd780
Prudvee <prudvee.gn@gmail.com>
2022-05-18 12:03:17 -0500

    linked payments to loan

---
+++ app/models/payment.rb
@@ -0,0 +1,21 @@
+class Payment < ActiveRecord::Base
+  belongs_to :loan
+
+  validates :amount, presence: true
+  validates :payment_date, presence: true
+  validate :amount_not_greater_than_outstanding_balance
+
+  def create(params)
+    byebug
+    params[:payment_date] = Time.at(params[:payment_date].to_i)
+    super
+  end
+
+  private
+
+  def amount_not_greater_than_outstanding_balance
+    return errors.add(:loan, "must be associated") if !loan
+    return true if amount <= loan.outstanding_balance
+    errors.add(:amount, "can't be larger than outstanding loan balance")
+  end
+end
